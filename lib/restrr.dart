01475723a7d242e8ded7adaa8a8dd6e4ac4ec02c
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-02-18 13:35:48 +0100

    Cleaned up restrr.dart

--- lib/restrr.dart
+++ lib/restrr.dart
@@ -1,25 +1,24 @@
 library;
 
-/* [ /src/entities ] */
+/* [ /src ] */
+export 'src/entity_builder.dart';
+export 'src/restrr_base.dart';
 
+/* [ /src/entities ] */
 export 'src/entities/health_response.dart';
 export 'src/entities/user.dart';
-/* [ /src ] */
 
-export 'src/entity_builder.dart';
-/* [ /src/requests/responses ] */
-
-export 'src/requests/responses/error_response.dart';
-export 'src/requests/responses/rest_response.dart';
 /* [ /src/requests ] */
-
 export 'src/requests/route.dart';
 export 'src/requests/route_definitions.dart';
-export 'src/restrr_base.dart';
-/* [ /src/service ] */
 
+/* [ /src/requests/responses ] */
+export 'src/requests/responses/error_response.dart';
+export 'src/requests/responses/rest_response.dart';
+
+/* [ /src/service ] */
 export 'src/service/api_service.dart';
 export 'src/service/user_service.dart';
-/* [ /src/utils ] */
 
+/* [ /src/utils ] */
 export 'src/utils/string_utils.dart';
