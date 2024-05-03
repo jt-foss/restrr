f75a34839cfdc8880b39c406d853edfcdcfdfddf
Jason Dean Lessenich <jasonlessenich@gmail.com>
2024-05-03 21:58:33 +0200

    Fixed use of EntityId<E> (#22)

--- lib/src/internal/cache/entity_cache_view_impl.dart
+++ lib/src/internal/cache/entity_cache_view_impl.dart
@@ -1,25 +1,25 @@
 import 'package:restrr/restrr.dart';
 
 class EntityCacheViewImpl<E extends RestrrEntity<E, ID>, ID extends EntityId<E>> implements EntityCacheView<E, ID> {
-  final Map<ID, E> _cache = {};
+  final Map<Id, E> _cache = {};
 
   @override
-  E? get(ID id) => _cache[id];
+  E? get(ID id) => _cache[id.value];
 
   @override
   List<E> getAll() => _cache.values.toList();
 
   @override
-  E add(E entity) => _cache[entity.id] = entity;
+  E add(E entity) => _cache[entity.id.value] = entity;
 
   @override
-  E? remove(ID id) => _cache.remove(id);
+  E? remove(ID id) => _cache.remove(id.value);
 
   @override
   void removeWhere(bool Function(E p1) predicate) => _cache.removeWhere((k, v) => predicate(v));
 
   @override
-  bool contains(ID id) => _cache.containsKey(id);
+  bool contains(ID id) => _cache.containsKey(id.value);
 
   @override
   void clear() => _cache.clear();
